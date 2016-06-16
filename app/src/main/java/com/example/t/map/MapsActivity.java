e50186f53c0d098c50ea115397b30dc323594da9
xyntherys <azmainadel47@gmail.com>
2016-06-17 04:14:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/t/map/MapsActivity.java
@@ -0,0 +1,36 @@
+package com.example.t.map;
+
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This shows how to create a simple activity with streetview
+ */
+public class MapsActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
+
+    // George St, Sydney
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.map);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+    }
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        panorama.setPosition(new LatLng(23.822211, 90.359411));
+    }
+}
+
